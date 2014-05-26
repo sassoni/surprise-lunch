bff550948dfa805524b9cc07b4572d93e4e40936
sassoni <chrysanthi.n@gmail.com>
2014-05-26 13:16:38 -0400

    Adding yelp request, adding gitignore

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+#from google.appengine.api import oauth
+import oauth2
+import logging
+import urllib
+import urllib2
+import json
+import ConfigParser
 
 class MainHandler(webapp2.RequestHandler):
+    def yelp_request(self, url_params):
+        # Read keys and tokens
+        config = ConfigParser.ConfigParser()
+        config.read('config.ini')
+        config_section = 'yelp_keys'
+        consumer_key = config.get(config_section, 'consumer_key')
+        consumer_secret = config.get(config_section, 'consumer_secret')
+        token = config.get(config_section, 'token')
+        token_secret = config.get(config_section, 'token_secret')
+        
+        # Unsigned URL
+        path = 'http://api.yelp.com/v2/search'
+        encoded_params = ''
+        if url_params:
+            encoded_params = urllib.urlencode(url_params)
+        url = '%s?%s' % (path, encoded_params)
+        #logging.info(url)
+
+        # Sign the URL
+        consumer = oauth2.Consumer(consumer_key, consumer_secret)
+        oauth_request = oauth2.Request('GET', url, {})
+        oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
+                        'oauth_timestamp': oauth2.generate_timestamp(),
+                        'oauth_token': token,
+                        'oauth_consumer_key': consumer_key})
+
+        token = oauth2.Token(token, token_secret)
+        oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+        signed_url = oauth_request.to_url()
+        logging.info(signed_url)
+
+        # Connect
+        try:
+            conn = urllib2.urlopen(signed_url, None)
+            try:
+                response = json.loads(conn.read())
+            finally:
+                conn.close()
+        except urllib2.HTTPError, error:
+            response = json.loads(error.read())
+
+        logging.info(response)
+        return response
+        
     def get(self):
         self.response.write('Hello world!')
-
+        # test request
+        response = self.yelp_request({'ll':'37.788022,-122.399797'})
+        
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
