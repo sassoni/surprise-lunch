521d33c04865f45a22de073b0a49f6669ce5bfae
sassoni <chrysanthi.n@gmail.com>
2014-06-20 22:33:56 -0400

    Changed spinner timing, function renaming

--- js/all-funcs.js
+++ js/all-funcs.js
@@ -1,3 +1,4 @@
+
 var opts = {
           lines: 13, // The number of lines to draw
           length: 30, // The length of each line
@@ -17,140 +18,137 @@ var opts = {
           left: '50%' // Left position relative to parent
         };
 
-        
-        var res_container = document.getElementById("restaurant-container");
-        var res_name = document.getElementById("restaurant_name");
-        var res_kind = document.getElementById("restaurant_kind");
-        var res_address = document.getElementById("restaurant_address");
-        var spinner = document.getElementById('loading-indicator');
-        var spin = new Spinner();
+var res_info = document.getElementById("restaurant_info");
+var res_name = document.getElementById("restaurant_name");
+var res_kind = document.getElementById("restaurant_kind");
+var res_address = document.getElementById("restaurant_address");
+var loading_indicator = document.getElementById('loading_indicator');
+var spinner = new Spinner();
             
-        var current_restaurant = {}    
-        var restaurants_list = {}
+var current_restaurant = {}    
+var restaurants_list = {}
         
-        var location_btn = document.getElementById("location_btn");
-        location_btn.addEventListener('click', getLocation);
+var location_btn = document.getElementById("location_btn");
+location_btn.addEventListener('click', getLocation);
         
-        var yes_btn = document.getElementById("yes_btn");
-        yes_btn.addEventListener('click', show_restaurant_address);
+var yes_btn = document.getElementById("yes_btn");
+yes_btn.addEventListener('click', show_restaurant_address);
         
-        var no_btn = document.getElementById("no_btn");
-        no_btn.addEventListener('click', choose_random_restaurant);
+var no_btn = document.getElementById("no_btn");
+no_btn.addEventListener('click', choose_random_restaurant);
         
-        function getLocation() {
-            // Hide 
-            hideAll();
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(sendLocation, handle_error);
-            } else {
-                res_name.innerHTML = "Geolocation is not supported by this browser.";
-            }
-        }
+function getLocation() {
+    hide_info_elems();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(sendLocation, handle_error);
+        spinner.spin();
+        loading_indicator.appendChild(spinner.el);
+    } else {
+         show_location_error();
+    }
+}
         
-        function sendLocation(position) {     
-            var location = {}
-            location['latitude'] = position.coords.latitude
-            location['longitude'] = position.coords.longitude
+function sendLocation(position) {     
+    var location = {}
+    location['latitude'] = position.coords.latitude
+    location['longitude'] = position.coords.longitude
                 
-            $.ajax({
-               url: '/',
-                type: 'POST',
-                data: JSON.stringify(location),
-                contentType: 'application/json',
-                success: on_request_success,
-                error: on_request_error  
-            });	
-        }
-        
-        $(document).ajaxSend(function(event, request, settings) {
-            //$('#loading-indicator').show();
-            spin.spin();
-            spinner.appendChild(spin.el);
-        });
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        data: JSON.stringify(location),
+        contentType: 'application/json',
+        success: on_request_success,
+        error: on_request_error  
+    });
+}
 
-        $(document).ajaxComplete(function(event, request, settings) {
-            //$('#loading-indicator').hide();
-            spin.stop();
-        });
-        
-        function handle_error(err) {
-            if (err.code == 1) {
-                console.debug('PERMISSION_DENIED');
-                res_name.innerHTML = "Permission denied";
-            } else if (err.code == 2) {
-                console.debug('POSITION UNAVAILABLE');
-                res_name.innerHTML = "Position unavailable";
-            } else if (err.code == 3) {
-                console.debug('TIMEOUT');
-                res_name.innerHTML = "timeout";
-            } else {
-                console.debug('SOMETHING ELSE WENT WRONG GETTING GEOLOCATION');
-                res_name.innerHTML = "Something went wrong";
-            }
-        }
-        
-        function choose_random_restaurant() { 
-            hide_restaurant_address();
-            var index_chosen = choose_random_restaurant_index();    
+$(document).ajaxComplete(function(event, request, settings) {
+    spinner.stop();
+});
+      
+function show_location_error() {
+    show_info_elems();
+    res_name.innerHTML = "Sorry, could not get your location :(";
+}
+      
+function handle_error(err) {
+    show_location_error();
             
-            if (index_chosen != -1) {
-                current_restaurant = restaurants_list[index_chosen]
-                show_restaurant();
-                restaurants_list.splice(index_chosen, 1);
-            } else {
-                current_restaurant = {}
-                res_name.innerHTML = "Sorry, no restaurants left :("
-                res_kind.innerHTML = "";
-                document.getElementById("no_btn").style.display = "none";
-                document.getElementById("yes_btn").style.display = "none";
-            }
-        }
+    // temp debug
+    if (err.code == 1) {
+        console.debug('PERMISSION_DENIED');
+    } else if (err.code == 2) {
+        console.debug('POSITION UNAVAILABLE');
+    } else if (err.code == 3) {
+        console.debug('TIMEOUT');
+    } else {
+        console.debug('SOMETHING ELSE WENT WRONG GETTING GEOLOCATION');
+    }
+}
         
-        function show_restaurant() {
-            var all_categories = [];
-            for (var i = 0; i < current_restaurant.categories.length; i++) { 
-                all_categories.push(current_restaurant.categories[i][0]);
-            }  
+function choose_random_restaurant() { 
+    hide_restaurant_address();
+    var index_chosen = choose_random_restaurant_index();    
             
-            res_name.innerHTML = current_restaurant.name
-            res_kind.innerHTML = all_categories.join(", ");
-        }
+    if (index_chosen != -1) {
+        current_restaurant = restaurants_list[index_chosen]
+        show_restaurant();
+        restaurants_list.splice(index_chosen, 1);
+    } else {
+        current_restaurant = {}
+        res_name.innerHTML = "Sorry, no restaurants left :("
+        res_kind.innerHTML = "";
+        document.getElementById("no_btn").style.display = "none";
+        document.getElementById("yes_btn").style.display = "none";
+    }
+}
         
-        function choose_random_restaurant_index() {
-            if (restaurants_list.length != 0) {
-                return Math.floor((Math.random() * restaurants_list.length));                   
-            } else {
-                return -1;
-            }
-        }
-        
-        function show_restaurant_address() {
-            res_address.innerHTML = current_restaurant.location.display_address;
-        }
-        
-        function hide_restaurant_address() {
-            res_address.innerHTML = "";
-        }
-        
-        function showAll() {
-            res_container.style.display = 'block';
-        }
-        
-        function hideAll() {
-            res_container.style.display = 'none';
-        }
-        
-        function on_request_success(response) {
-            showAll();
-            document.getElementById("no_btn").style.display = "inline";
-            document.getElementById("yes_btn").style.display = "inline";
+function show_restaurant() {
+    var all_categories = [];
+    for (var i = 0; i < current_restaurant.categories.length; i++) { 
+        all_categories.push(current_restaurant.categories[i][0]);
+    }  
             
-            obj = JSON.parse(response);
-            restaurants_list = obj.businesses
-            console.debug('restaurants', restaurants_list)
-            choose_random_restaurant()
-        } 
+    res_name.innerHTML = current_restaurant.name
+    res_kind.innerHTML = all_categories.join(", ");
+}
+        
+function choose_random_restaurant_index() {
+    if (restaurants_list.length != 0) {
+        return Math.floor((Math.random() * restaurants_list.length));                   
+    } else {
+        return -1;
+    }
+}
+        
+function show_restaurant_address() {
+    res_address.innerHTML = current_restaurant.location.display_address;
+}
+        
+function hide_restaurant_address() {
+    res_address.innerHTML = "";
+}
+        
+function show_info_elems() {
+    res_info.style.display = 'block';
+}
+        
+function hide_info_elems() {
+    res_info.style.display = 'none';
+}
+        
+function on_request_success(response) {
+    show_info_elems();
+    document.getElementById("no_btn").style.display = "inline";
+    document.getElementById("yes_btn").style.display = "inline";
             
-        function on_request_error(r, text_status, error_thrown) {
-            console.debug('error', text_status + ", " + error_thrown + ":\n" + r.responseText);
-        }
+    obj = JSON.parse(response);
+    restaurants_list = obj.businesses
+    console.debug('restaurants', restaurants_list)
+    choose_random_restaurant()
+} 
+            
+function on_request_error(r, text_status, error_thrown) {
+    console.debug('error', text_status + ", " + error_thrown + ":\n" + r.responseText);
+}
